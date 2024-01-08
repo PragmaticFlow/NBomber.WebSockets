60e38de885f7e71f7f08896728943da18efbbee7
antyadev <antyadev@gmail.com>
2024-01-08 22:26:39 +0200

    init commit

---
+++ src/NBomber.WebSockets/WebSocket.cs
@@ -0,0 +1,76 @@
+﻿using System.Net.WebSockets;
+using Microsoft.IO;
+
+namespace NBomber.WebSockets;
+
+public class WebSocketConfig
+{
+    public int DefaultBufferSize { get; set; } = 16384; // 16KB
+}
+
+/// <summary>
+/// Provides a client for connecting to WebSocket services.
+/// WebSocket type is a wrapper over native .NET <see cref="ClientWebSocket"/> type.
+/// </summary>
+public class WebSocket(WebSocketConfig config) : IDisposable
+{
+    private static readonly RecyclableMemoryStreamManager MsStreamManager = new();
+
+    public ClientWebSocket Client { get; } = new();
+
+    /// <summary>
+    /// Asynchronously receives a WebSocket message from the connected WebSocket.
+    /// This method returns <see cref="WebSocketResponse"/> that should be disposed of after usage.
+    /// </summary>
+    /// <param name="cancellationToken">A <see cref="CancellationToken"/> used to propagate notification that the operation should be canceled.</param>
+    /// <exception cref="WebSocketException">Thrown when an error occurs during WebSocket communication.</exception>
+    /// <exception cref="OperationCanceledException">Thrown if the receive operation is canceled by the provided cancellation token.</exception>
+    public async ValueTask<WebSocketResponse> Receive(CancellationToken? cancellationToken = null)
+    {
+        var endOfMessage = false;
+        var ms = MsStreamManager.GetStream();
+        var msgType = WebSocketMessageType.Binary;
+
+        try
+        {
+            while (!endOfMessage)
+            {
+                var buffer = ms.GetMemory(config.DefaultBufferSize);
+                var message = await Client.ReceiveAsync(buffer, cancellationToken ?? CancellationToken.None).ConfigureAwait(false);
+
+                if (message.MessageType == WebSocketMessageType.Close)
+                {
+                    return new WebSocketResponse(ms, WebSocketMessageType.Close);
+                }
+
+                ms.Advance(message.Count);
+
+                endOfMessage = message.EndOfMessage;
+                msgType = message.MessageType;
+            }
+
+            return new WebSocketResponse(ms, msgType);
+        }
+        catch
+        {
+            ms.Dispose();
+            throw;
+        }
+    }
+
+    public void Dispose()
+    {
+        Client.Dispose();
+    }
+}
+
+public class WebSocketResponse(RecyclableMemoryStream memoryStream, WebSocketMessageType messageType) : IDisposable
+{
+    public ReadOnlyMemory<byte> Data { get; } = memoryStream.GetBuffer().AsMemory(0, (int)memoryStream.Length);
+    public WebSocketMessageType MessageType { get; } = messageType;
+
+    public void Dispose()
+    {
+        memoryStream.Dispose();
+    }
+}
---
+++ src/NBomber.WebSockets/WebSocketExtensions.cs
@@ -0,0 +1,78 @@
+using System.Net.WebSockets;
+using System.Text;
+using Microsoft.IO;
+
+namespace NBomber.WebSockets;
+
+public static class WebSocketExtensions
+{
+    private static readonly RecyclableMemoryStreamManager MsStreamManager = new();
+
+    /// <summary>
+    /// Connects to a WebSocket server as an asynchronous operation.
+    /// </summary>
+    /// <param name="url">The URL of the WebSocket server to connect to.</param>
+    /// <param name="cancellationToken">A <see cref="CancellationToken"/> used to propagate notification that the operation should be canceled.</param>
+    /// <exception cref="WebSocketException">Thrown when an error occurs during WebSocket communication.</exception>
+    /// <exception cref="OperationCanceledException">Thrown if the receive operation is canceled by the provided cancellation token.</exception>
+    public static Task Connect(this WebSocket webSocket, string url, CancellationToken? cancellationToken = null)
+    {
+        return webSocket.Client.ConnectAsync(new Uri(url), cancellationToken ?? CancellationToken.None);
+    }
+
+    /// <summary>
+    /// Connects to a WebSocket server as an asynchronous operation.
+    /// </summary>
+    /// <param name="uri">The URI of the WebSocket server to connect to.</param>
+    /// <param name="cancellationToken">A <see cref="CancellationToken"/> used to propagate notification that the operation should be canceled.</param>
+    /// <exception cref="WebSocketException">Thrown when an error occurs during WebSocket communication.</exception>
+    /// <exception cref="OperationCanceledException">Thrown if the receive operation is canceled by the provided cancellation token.</exception>
+    public static Task Connect(this WebSocket webSocket, Uri uri, CancellationToken? cancellationToken = null)
+    {
+        return webSocket.Client.ConnectAsync(uri, cancellationToken ?? CancellationToken.None);
+    }
+
+    /// <summary>
+    /// Close the WebSocket instance as an asynchronous operation.
+    /// </summary>
+    /// <param name="closeStatus">The WebSocket close status.</param>
+    /// <param name="cancellationToken">A <see cref="CancellationToken"/> used to propagate notification that the operation should be canceled.</param>
+    /// <exception cref="WebSocketException">Thrown when an error occurs during WebSocket communication.</exception>
+    /// <exception cref="OperationCanceledException">Thrown if the receive operation is canceled by the provided cancellation token.</exception>
+    public static Task Close(this WebSocket webSocket, WebSocketCloseStatus? closeStatus = null, CancellationToken? cancellationToken = null)
+    {
+        return webSocket.Client.CloseAsync(closeStatus ?? WebSocketCloseStatus.NormalClosure, null, cancellationToken ?? CancellationToken.None);
+    }
+
+    /// <summary>
+    /// Sends data on WebSocket as an asynchronous operation.
+    /// </summary>
+    /// <param name="text">The text to be sent. It will be encoded using UTF8 encoding.</param>
+    /// <param name="cancellationToken">A <see cref="CancellationToken"/> used to propagate notification that the operation should be canceled.</param>
+    /// <exception cref="WebSocketException">Thrown when an error occurs during WebSocket communication.</exception>
+    /// <exception cref="OperationCanceledException">Thrown if the receive operation is canceled by the provided cancellation token.</exception>
+    public static ValueTask Send(this WebSocket webSocket, string text, CancellationToken? cancellationToken = null)
+    {
+        using var ms = MsStreamManager.GetStream();
+
+        var buffer = ms.GetMemory(text.Length);
+        var byteCount = Encoding.UTF8.GetBytes(text.AsSpan(), buffer.Span);
+        ms.Advance(byteCount);
+
+        var msg = ms.GetBuffer().AsMemory(0, (int) ms.Length);
+
+        return webSocket.Client.SendAsync(msg, WebSocketMessageType.Text, true, cancellationToken ?? CancellationToken.None);
+    }
+
+    /// <summary>
+    /// Sends data on WebSocket as an asynchronous operation.
+    /// </summary>
+    /// <param name="payload">The binary payload to be sent.</param>
+    /// <param name="cancellationToken">A <see cref="CancellationToken"/> used to propagate notification that the operation should be canceled.</param>
+    /// <exception cref="WebSocketException">Thrown when an error occurs during WebSocket communication.</exception>
+    /// <exception cref="OperationCanceledException">Thrown if the receive operation is canceled by the provided cancellation token.</exception>
+    public static ValueTask Send(this WebSocket webSocket, ReadOnlyMemory<byte> payload, CancellationToken? cancellationToken = null)
+    {
+        return webSocket.Client.SendAsync(payload, WebSocketMessageType.Binary, true, cancellationToken ?? CancellationToken.None);
+    }
+}
